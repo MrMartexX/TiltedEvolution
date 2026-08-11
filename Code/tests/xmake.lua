4f61863ec4b12671b4c9697d449dbc35c5330c96
MrMartexX <152758332+MrMartexX@users.noreply.github.com>
2026-08-11 23:28:33 +0300

    Remove conditional runtime session test ABI

--- Code/tests/xmake.lua
+++ Code/tests/xmake.lua
@@ -4,7 +4,6 @@ target("TPTests")
     set_group("Tests")
     add_includedirs(
         ".", "../encoding")
-    add_defines("TP_PARTY_QUEST_LOW_LEVEL_TEST_ACCESS")
     add_headerfiles("**.h")
     add_files("*.cpp")
     if is_plat("windows") then
