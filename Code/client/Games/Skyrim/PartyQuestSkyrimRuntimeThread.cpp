3afac3f99e6539b664451d7a89eec56b7745c8d2
MrMartexX <152758332+MrMartexX@users.noreply.github.com>
2026-08-12 12:07:23 +0300

    Implement fail-closed Skyrim update thread binding

---
+++ Code/client/Games/Skyrim/PartyQuestSkyrimRuntimeThread.cpp
@@ -0,0 +1,41 @@
+#include <TiltedOnlinePCH.h>
+
+#include <PartyQuestSkyrimRuntimeThread.h>
+
+#include <atomic>
+
+namespace
+{
+std::atomic<uint32_t> s_runtimeUpdateThreadId{0};
+}
+
+bool PartyQuestSkyrimRuntimeThread::ObserveCurrentUpdateThread() noexcept
+{
+    const uint32_t currentThreadId = GetCurrentThreadId();
+    if (currentThreadId == 0)
+        return false;
+
+    uint32_t expected = 0;
+    if (s_runtimeUpdateThreadId.compare_exchange_strong(
+            expected,
+            currentThreadId,
+            std::memory_order_acq_rel,
+            std::memory_order_acquire))
+    {
+        return true;
+    }
+
+    return expected == currentThreadId;
+}
+
+bool PartyQuestSkyrimRuntimeThread::IsCurrentUpdateThread() noexcept
+{
+    const uint32_t boundThreadId =
+        s_runtimeUpdateThreadId.load(std::memory_order_acquire);
+    return boundThreadId != 0 && boundThreadId == GetCurrentThreadId();
+}
+
+uint32_t PartyQuestSkyrimRuntimeThread::GetBoundThreadIdForDiagnostics() noexcept
+{
+    return s_runtimeUpdateThreadId.load(std::memory_order_acquire);
+}
