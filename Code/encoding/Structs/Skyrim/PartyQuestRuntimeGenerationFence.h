f59888204cf263b5610a9af68f7b63b5d83cce7c
MrMartexX <152758332+MrMartexX@users.noreply.github.com>
2026-08-12 09:34:25 +0300

    Bind quest mutation dispatch to runtime generation

---
+++ Code/encoding/Structs/Skyrim/PartyQuestRuntimeGenerationFence.h
@@ -0,0 +1,51 @@
+#pragma once
+
+#include <cstdint>
+#include <optional>
+#include <shared_mutex>
+
+class PartyQuestRuntimeGenerationFence final
+{
+public:
+    class ExecutionLease final
+    {
+    public:
+        ExecutionLease(ExecutionLease&&) noexcept = default;
+        ExecutionLease& operator=(ExecutionLease&&) noexcept = default;
+        ExecutionLease(const ExecutionLease&) = delete;
+        ExecutionLease& operator=(const ExecutionLease&) = delete;
+
+        [[nodiscard]] uint64_t GetGeneration() const noexcept
+        {
+            return m_generation;
+        }
+
+        [[nodiscard]] bool IsValid() const noexcept
+        {
+            return m_lock.owns_lock() && m_generation != 0;
+        }
+
+    private:
+        friend class PartyQuestRuntimeGenerationFence;
+
+        ExecutionLease(
+            std::shared_lock<std::shared_mutex>&& aLock,
+            uint64_t aGeneration) noexcept
+            : m_lock(std::move(aLock))
+            , m_generation(aGeneration)
+        {
+        }
+
+        std::shared_lock<std::shared_mutex> m_lock;
+        uint64_t m_generation{};
+    };
+
+    [[nodiscard]] uint64_t GetGeneration() const noexcept;
+    [[nodiscard]] uint64_t Invalidate() noexcept;
+    [[nodiscard]] std::optional<ExecutionLease> TryAcquire(
+        uint64_t aExpectedGeneration) const noexcept;
+
+private:
+    mutable std::shared_mutex m_mutex;
+    uint64_t m_generation{1};
+};
