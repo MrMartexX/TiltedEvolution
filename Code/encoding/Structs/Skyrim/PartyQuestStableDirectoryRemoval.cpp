8b431d21a976df7d933da46392bf3e7c32f30c52
MrMartexX <152758332+MrMartexX@users.noreply.github.com>
2026-08-19 12:44:29 +0300

    Implement durable empty-directory removal

---
+++ Code/encoding/Structs/Skyrim/PartyQuestStableDirectoryRemoval.cpp
@@ -0,0 +1,46 @@
+#include <Structs/Skyrim/PartyQuestStableStorage.h>
+
+#include <system_error>
+
+#ifndef _WIN32
+#include <sys/stat.h>
+#include <unistd.h>
+#endif
+
+PartyQuestStableStorageStatus PartyQuestStableStorage::RemoveEmptyDirectoryDurably(
+    const std::filesystem::path& acDirectory) noexcept
+{
+    if (acDirectory.empty())
+        return PartyQuestStableStorageStatus::InvalidPath;
+
+#ifdef _WIN32
+    return PartyQuestStableStorageStatus::Unsupported;
+#else
+    try
+    {
+        std::error_code ec;
+        const auto directory =
+            std::filesystem::absolute(acDirectory, ec).lexically_normal();
+        if (ec || directory.empty() || directory.parent_path().empty() ||
+            directory == directory.root_path())
+        {
+            return PartyQuestStableStorageStatus::InvalidPath;
+        }
+
+        struct stat status{};
+        if (::lstat(directory.c_str(), &status) != 0)
+            return PartyQuestStableStorageStatus::OpenFailed;
+        if (!S_ISDIR(status.st_mode))
+            return PartyQuestStableStorageStatus::NodeValidationFailed;
+
+        if (::rmdir(directory.c_str()) != 0)
+            return PartyQuestStableStorageStatus::RemoveFailed;
+
+        return FlushDirectory(directory.parent_path());
+    }
+    catch (...)
+    {
+        return PartyQuestStableStorageStatus::InvalidPath;
+    }
+#endif
+}
